87fbbaef851bbead08c706ec31de246e43003878
Connor Hitchcock <fch58@uclive.ac.nz>
2020-07-31 11:03:35 +1200

    Changed some thing in the test file as the JUnit tests weren't running. However that is fixed now.

--- src/test/java/seng202/team6/AppTest.java
+++ src/test/java/seng202/team6/AppTest.java
@@ -1,36 +1,15 @@
 package seng202.team6;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
@@ -39,6 +18,7 @@ public class AppTest
     /**
      * Callum's test
      */
+    @Test
     public void CallumTest()
     {
         int num = 5;
@@ -48,28 +28,30 @@ public class AppTest
     /**
      * Fletcher's test
      */
+    @Test
     public void FletchersTest()
     {
         int math = 2 + 2 - 1;
         // Quick math from Big Shaq
         assertEquals(3, math);
-        
+
     }
 
     /**
      * Rutger's test
      */
+    @Test
     public void RutgersTest()
     {
         int number = 9 + 10;
         // Twenny wan
         assertEquals(19, number);
-
     }
 
     /**
      * Omar's test
      */
+    @Test
     public void OmarsTest()
     {
         int number = 2;
@@ -79,9 +61,20 @@ public class AppTest
     /**
      * Joseph's test
      */
+    @Test
     public void JosephTest()
     {
         int number = 100;
         assertEquals(100, number);
+    }
+
+    /**
+     * Connor's test
+     */
+    @Test
+    public void ConnorTest()
+    {
+        int powerLevel = 9069;
+        assertTrue(powerLevel >= 9000);
     }
 }
