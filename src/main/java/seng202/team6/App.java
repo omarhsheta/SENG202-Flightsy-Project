45952a7019fca5a10b6564129b16ba891ec5ca7d
Callum McLoughlin <cwm62@uclive.ac.nz>
2020-08-09 22:22:28 +1200

    Added JavaFX with BootstrapFX CSS stylesheet to project.

--- src/main/java/seng202/team6/App.java
+++ src/main/java/seng202/team6/App.java
@@ -1,15 +1,17 @@
 package seng202.team6;
 
 import com.google.gson.Gson;
+import javafx.application.Application;
+import seng202.team6.gui.MainGUI;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args ) {
-
+    public static void main( String[] args )
+    {
         System.out.println( "Hwllo Seng202 team 6" );
 
         MyObject myObject = new MyObject( "chair", 3 );
@@ -18,5 +20,8 @@ public class App
 
         System.out.println( "myObject = " + myObject );
         System.out.println( "myObject stringfield = " + jsonString );
+
+        //Launch javaFX GUI
+        Application.launch(MainGUI.class, args);
     }
 }
---
+++ src/main/java/seng202/team6/gui/MainGUI.java
@@ -0,0 +1,35 @@
+package seng202.team6.gui;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
+public class MainGUI extends Application
+{
+    /*
+        This is the example BootstrapFX GUI
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Panel panel = new Panel("This is the title");
+        panel.getStyleClass().add("panel-primary");
+        BorderPane content = new BorderPane();
+        content.setPadding(new Insets(20));
+        Button button = new Button("Hello BootstrapFX");
+        button.getStyleClass().setAll("btn","btn-danger");
+        content.setCenter(button);
+        panel.setBody(content);
+
+        Scene scene = new Scene(panel);
+        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
+
+        primaryStage.setTitle("BootstrapFX");
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+}
