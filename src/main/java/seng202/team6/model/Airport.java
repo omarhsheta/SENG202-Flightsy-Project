90b6f356054bea57ab9ec527173b077293b06f4c
Connor Hitchcock <fch58@uclive.ac.nz>
2020-08-10 15:25:41 +1200

    Create skeleton code for the four main object classes including getters and setters.

---
+++ src/main/java/seng202/team6/model/Airport.java
@@ -0,0 +1,120 @@
+package seng202.team6.model;
+
+public class Airport {
+
+    int AirportID;
+    String Name;
+    String City;
+    String Country;
+    String IATA;
+    String ICAO;
+    float Latitude;
+    float Longitude;
+    int Altitude;
+    int Timezone;
+    char DST;
+
+    public Airport(int newAirportID, String newName, String newCity, String newCountry, String newIATA,
+                   String newICAO, float newLatitude, float newLongitude, int newAltitude, int newTimezone,
+                   char newDST) {
+        AirportID = newAirportID;
+        Name = newName;
+        City = newCity;
+        Country = newCountry;
+        IATA = newIATA;
+        ICAO = newICAO;
+        Latitude = newLatitude;
+        Longitude = newLongitude;
+        Altitude = newAltitude;
+        Timezone = newTimezone;
+        DST = newDST;
+    }
+
+    public int getAirportID() {
+        return AirportID;
+    }
+
+    public void setAirportID(int airportID) {
+        AirportID = airportID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getIATA() {
+        return IATA;
+    }
+
+    public void setIATA(String IATA) {
+        this.IATA = IATA;
+    }
+
+    public String getICAO() {
+        return ICAO;
+    }
+
+    public void setICAO(String ICAO) {
+        this.ICAO = ICAO;
+    }
+
+    public float getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        Latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        Longitude = longitude;
+    }
+
+    public int getAltitude() {
+        return Altitude;
+    }
+
+    public void setAltitude(int altitude) {
+        Altitude = altitude;
+    }
+
+    public int getTimezone() {
+        return Timezone;
+    }
+
+    public void setTimezone(int timezone) {
+        Timezone = timezone;
+    }
+
+    public char getDST() {
+        return DST;
+    }
+
+    public void setDST(char DST) {
+        this.DST = DST;
+    }
+}
---
+++ src/main/java/seng202/team6/model/Route.java
@@ -0,0 +1,100 @@
+package seng202.team6.model;
+
+public class Route {
+
+    int AirlineID;
+    String Airline;
+    String SourceAirport;
+    int SourceAirportID;
+    String DestinationAirport;
+    int DestinationAirportID;
+    char Codeshare;
+    int Stops;
+    String Equipment;
+
+    public Route(int newAirlineID, String newAirline, String newSourceAirport, int newSourceAirportID,
+                 String newDestinationAirport, int newDestinationAirportID, char newCodeshare, int newStops,
+                 String newEquipment) {
+        AirlineID = newAirlineID;
+        Airline = newAirline;
+        SourceAirport = newSourceAirport;
+        SourceAirportID = newSourceAirportID;
+        DestinationAirport = newDestinationAirport;
+        DestinationAirportID = newDestinationAirportID;
+        Codeshare = newCodeshare;
+        Stops = newStops;
+        Equipment = newEquipment;
+    }
+
+    public int getAirlineID() {
+        return AirlineID;
+    }
+
+    public void setAirlineID(int airlineID) {
+        AirlineID = airlineID;
+    }
+
+    public String getAirline() {
+        return Airline;
+    }
+
+    public void setAirline(String airline) {
+        Airline = airline;
+    }
+
+    public String getSourceAirport() {
+        return SourceAirport;
+    }
+
+    public void setSourceAirport(String sourceAirport) {
+        SourceAirport = sourceAirport;
+    }
+
+    public int getSourceAirportID() {
+        return SourceAirportID;
+    }
+
+    public void setSourceAirportID(int sourceAirportID) {
+        SourceAirportID = sourceAirportID;
+    }
+
+    public String getDestinationAirport() {
+        return DestinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        DestinationAirport = destinationAirport;
+    }
+
+    public int getDestinationAirportID() {
+        return DestinationAirportID;
+    }
+
+    public void setDestinationAirportID(int destinationAirportID) {
+        DestinationAirportID = destinationAirportID;
+    }
+
+    public char getCodeshare() {
+        return Codeshare;
+    }
+
+    public void setCodeshare(char codeshare) {
+        Codeshare = codeshare;
+    }
+
+    public int getStops() {
+        return Stops;
+    }
+
+    public void setStops(int stops) {
+        Stops = stops;
+    }
+
+    public String getEquipment() {
+        return Equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        Equipment = equipment;
+    }
+}
