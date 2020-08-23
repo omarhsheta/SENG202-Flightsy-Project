e4e33699cac21b21a02036e22c8634fcc502aee4
fletcher_dick <fletcherjdick@gmail.com>
2020-08-23 16:43:25 +1200

    Added controller for data window and linked airport table to controller

---
+++ src/main/java/seng202/team6/gui/controller/DataViewerController.java
@@ -0,0 +1,20 @@
+package seng202.team6.gui.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DataViewerController implements Initializable
+{
+    @FXML
+    private TableView airport_table;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    private void FillAirports() {
+    }
+}
