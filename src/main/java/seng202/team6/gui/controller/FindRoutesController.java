3c302448acede978cd5616b08656bb64ba54f519
Callum McLoughlin <cwm62@uclive.ac.nz>
2020-08-27 23:52:47 +1200

    Updated filtering GUI components and added filtering for routes between airports

--- src/main/java/seng202/team6/gui/controller/FindRoutesController.java
+++ src/main/java/seng202/team6/gui/controller/FindRoutesController.java
@@ -4,12 +4,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import seng202.team6.gui.components.FilterTextField;
 import seng202.team6.model.data.DataHandler;
 import seng202.team6.model.data.Filter;
+import seng202.team6.model.entities.Airport;
+import seng202.team6.model.entities.Route;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,11 +29,27 @@ public class FindRoutesController implements Initializable
     private final String mapHTML = "/map/main.html";
 
     @FXML
-    private Pane resultsPane;
+    private VBox resultsPane;
 
+    //Airport filtering
     @FXML
-    private Pane filterPane;
-    private final ArrayList<FilterTextField> filterTextFields = new ArrayList<>();
+    private VBox airportFilterBox;
+    private ArrayList<FilterTextField> airportFilterTextFields;
+
+
+    //Flight filtering
+    @FXML
+    private VBox flightFilterOriginBox;
+    @FXML
+    private VBox flightFilterDestinationBox;
+    @FXML
+    private Slider maxStopsSlider;
+    private ArrayList<FilterTextField> flightFilterOriginTextFields;
+    private ArrayList<FilterTextField> flightFilterDestinationTextFields;
+
+    //Route filtering
+    @FXML
+    private VBox routeFilterBox;
 
     private MapController controller;
 
@@ -45,27 +66,97 @@ public class FindRoutesController implements Initializable
         controller = new MapController(webEngine);
 
         //Grab all text filter components
-        for (Node node : filterPane.getChildren()) {
-            if (node != null && node.getClass() == FilterTextField.class) {
-                filterTextFields.add((FilterTextField) node);
-            }
-        }
+        airportFilterTextFields = GetAllNodes(airportFilterBox);
+        flightFilterOriginTextFields = GetAllNodes(flightFilterOriginBox);
+        flightFilterDestinationTextFields = GetAllNodes(flightFilterDestinationBox);
     }
 
-    @FXML
-    private void OnFilterButtonClicked() {
+    /**
+     * Grab all filter box nodes from VBox
+     * @param box Component to get all child filter text fields from
+     * @return ArrayList of filter text fields
+     */
+    private ArrayList<FilterTextField> GetAllNodes(VBox box) {
+        ArrayList<FilterTextField> nodes = new ArrayList<>();
+        for (Node node : box.getChildren()) {
+            if (node != null && node.getClass() == FilterTextField.class) {
+                nodes.add((FilterTextField) node);
+            }
+        }
+        return nodes;
+    }
+
+    /**
+     * Extract filters from filter text GUI components
+     * @param filterFields List of filter GUI components
+     * @return List of filters
+     */
+    private ArrayList<Filter> ExtractFilters(ArrayList<FilterTextField> filterFields) {
         ArrayList<Filter> filters = new ArrayList<>();
-        for (FilterTextField box : this.filterTextFields) {
+        for (FilterTextField box : filterFields) {
             if (!box.getText().equals("")) {
                 String filterString = String.format(box.GetFilter(), box.getText());
                 filters.add(new Filter(filterString, "AND"));
             }
         }
+        return filters;
+    }
 
+    /**
+     * Draw airport marks based on filters
+     */
+    @FXML
+    private void OnAirportFilterButtonClicked() {
+        ArrayList<Filter> filters = ExtractFilters(this.airportFilterTextFields);
         try {
             controller.ClearAll();
             controller.DrawAirportMarks(DataHandler.GetInstance().FetchAirports(filters));
         } catch (Exception ignored) {
         }
+    }
+
+    /**
+     * Get all routes between source and destination airports
+     */
+    @FXML
+    private void OnFlightFilterButtonClicked() {
+        ArrayList<Filter> originFilters = ExtractFilters(this.flightFilterOriginTextFields);
+        ArrayList<Filter> destinationFilters = ExtractFilters(this.flightFilterDestinationTextFields);
+
+        ArrayList<Route> routes;
+        ArrayList<Airport> sourceAirports;
+        ArrayList<Airport> destinationAirports;
+
+        try {
+            sourceAirports = DataHandler.GetInstance().FetchAirports(originFilters);
+            destinationAirports = DataHandler.GetInstance().FetchAirports(destinationFilters);
+            routes = DataHandler.GetInstance().FetchRoutes(sourceAirports, destinationAirports);
+        } catch (Exception ignored) {
+            return;
+        }
+
+        //VERY TEMPORARY TO TEST
+        ArrayList<Airport> airportsToDraw = new ArrayList<>();
+        sourceAirports.addAll(destinationAirports);
+        resultsPane.getChildren().clear();
+        for (Route route : routes) {
+            for (Airport airport : sourceAirports) {
+                if (airport.GetIATA().equals(route.GetSourceAirport()) || airport.GetIATA().equals(route.GetDestinationAirport())) {
+                    airportsToDraw.add(airport);
+
+                    TextArea text = new TextArea();
+                    text.setText(String.format("%s --> %s", route.GetSourceAirport(), route.GetDestinationAirport()));
+                    resultsPane.getChildren().add(text);
+                }
+            }
+        }
+        controller.ClearAll();
+        controller.DrawAirportMarks(airportsToDraw);
+        //END TEMPORARY
+    }
+
+    @FXML
+    private void OnRouteFilterButtonClicked() {
+
     }
 }
