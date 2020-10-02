66a11bc9376d15493e978985cab62b91ca8340f3
osh18 <osh18@uclive.ac.nz>
2020-10-02 20:48:06 +1300

    Okay I fixed it, by fixed it, I mean I made it 'committable' if that is even a word. But Cucumber is far from functional

--- src/test/java/cucumber/FlightStepDefinition.java
+++ src/test/java/cucumber/FlightStepDefinition.java
@@ -7,8 +7,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="resources/cucumber", glue="")
+//@RunWith(Cucumber.class)
+@CucumberOptions(glue="seng202-team6/src/test/java/cucumber", //MAY NEED TO TINKER WITH THAT DELETE ME IF FIXED OK BYE NOW
+        plugin = {"pretty", "html:target/cucumber.html"},snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class FlightStepDefinition {
 
     @Given("the user {String} has ${int} total flights")
