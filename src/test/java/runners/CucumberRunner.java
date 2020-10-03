e163a03858a1b08af8dffbe39a4c42e01b1c1d0d
Callum McLoughlin <cwm62@uclive.ac.nz>
2020-10-03 19:32:50 +1300

    Fixed cucumber :^)

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="seng202-team6/src/test/resources/cucumber",
+@CucumberOptions(features="src/test/resources/cucumber",
         plugin = {"pretty", "html:target/cucumber.html"},
         snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class CucumberRunner {
